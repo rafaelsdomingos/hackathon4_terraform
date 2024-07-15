6fcee79187e167f22bc6e9bc869c682c009c951a
rafaelsdomingos <rafaelsdomingos@gmail.com>
2024-07-14 22:31:09 -0300

    Cria output.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "hackathon4" {
   ami           = "ami-06c68f701d8090592"
   instance_type = var.instance_type
 
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "instance_public_ip"{
+    description = "Ip publico da instancia"
+    value = aws_instance.hackathon4.public.ip
+}
