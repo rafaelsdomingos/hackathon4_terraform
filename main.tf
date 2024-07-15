324919680ef1cfd5532c26057315f1cb39e43a61
rafaelsdomingos <rafaelsdomingos@gmail.com>
2024-07-14 22:23:34 -0300

    Cria o arquivo variable.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-06c68f701d8090592"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "Hackathon4"
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "instance_type" {
+    description = "Tipo de instancia EC2 a ser provisionada"
+    default = "t2.micro"
+}
